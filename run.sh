a65e3f00507dc352b7eb1e6c831c3bd998abf6bc
jalknThoth <alejalkndro@gmail.com>
2024-12-12 12:30:13 -0500

    introDjango

--- run.sh
+++ run.sh
@@ -1,3 +1,24 @@
+#check python version
+python --version
+pip --version
+
+#make virtual envoriment
 python3 -m venv .venv
+
+#activate virtual environment
+source .venv/bin/activate
+
+#install dajango
 pip install django
-django-admin startproject demo
+
+#start a project named demo
+django-admin startproject 'projectName'
+
+#change directory to demo
+cd 'projectName'
+
+#start new app named myapp
+python3 manage.py startapp members
+
+#run the server
+python3 manage.py runserver
